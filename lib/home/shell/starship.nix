24d979b54060619042350b4cefafc44b94801681
Rhys Scandlyn <rhys.scandlyn@gmail.com>
2026-08-22 12:08:37 +1200

    fix(starship): disable automatic zsh integration in favor of custom initContent

--- lib/home/shell/starship.nix
+++ lib/home/shell/starship.nix
@@ -6,7 +6,7 @@
 
   programs.starship = {
     enable = true;
-    enableZshIntegration = true;
+    enableZshIntegration = false;
     configPath = "${config.xdg.configHome}/starship/starship.toml";
     #settings = {
     #  add_newline = false;
